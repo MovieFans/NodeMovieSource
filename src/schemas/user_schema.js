f427ad644011b9890b15d41200b58796e08e9987
Roderick <ten.thousand@foxmail.com>
2016-02-21 16:12:18 +0800

    local commit

--- src/schemas/user_schema.js
+++ src/schemas/user_schema.js
@@ -1,3 +1,32 @@
 /**
  * Created by Roderick on 2016/2/20.
  */
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+	name: {
+		unique: true,
+		type: String
+	},
+	password: String,
+	//- 0: nomal
+	//- 1: verified
+	//- 2: professonal
+	//- >10: admin
+	//- >50: superAdmin
+	role: {
+		type: Number,
+		default: 0
+	},
+	meta: {
+		createAt: {
+			type: Date,
+			default: Date.now()
+		},
+		updateAt: {
+			type: Date,
+			default: Date.now()
+		}
+	}
+
+})
