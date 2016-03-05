25531e865afce9fd84a3845902331dcae6ec4a76
lily <1063609531@qq.com>
2016-03-05 23:48:51 +0800

    MovieSchema.js add

--- src/schemas/movie_schema.js
+++ src/schemas/movie_schema.js
@@ -2,3 +2,46 @@
  * Created by Judith on 2016/3/4.
  */
 var mongoose = require('mongoose');
+var bcrypt = require('bctypt-nodejs');
+
+var MovieSchema = new mongoose.Schema({
+	movieName:{
+		unique: true,
+		type: String
+	},
+	director:String,
+	writers:String,
+	actors:String,
+	type:String,
+	countries:String,
+	language:String,
+	meta: {
+		createAt: {
+			type: Date,
+			default: Date.now()
+		},
+		updateAt: {
+			type: Date,
+			default: Date.now()
+		},
+		showDate:{
+			type:Date,
+			default: Date.now()
+		}
+	},
+	runtime:String
+});
+
+MovieSchema.pre('save',function(next){
+	var movie = this;
+
+	if(this.isNew){
+		this.meta.createAt = this.meta.updateAt = Date.now();
+	}else{
+		this.meta.updateAt = Date.now();
+	}
+
+	next();
+});
+
+module.exports = MovieSchema;
