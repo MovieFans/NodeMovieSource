3d2a7341361ba8d08cd941aae9f1f0d2cacd2e84
Roderick2015 <ten.thousand@foxmail.com>
2016-08-23 10:38:44 +0800

    add thrift client

--- src/remote/cinema_info.js
+++ src/remote/cinema_info.js
@@ -2,27 +2,30 @@
  * Created by Roderick on 2016/8/22.
  */
 var thrift =require('thrift');
-/*var ThriftTransports = require('thrift/transport');
-var ThriftProtocols = require('thrift/protocol');*/
 var hello = require('./HelloWorld');
-var helloType = require('./HelloWorld_types');
+//var helloType = require('./HelloWorld_types');
 
-/*transport = ThriftTransports.TBufferedTransport()
-protocol = ThriftProtocols.TBinaryProtocol()*/
+var options = {
+    transport: thrift.TFramedTransport,
+    protocol: thrift.TBinaryProtocol,
+    path: "/HelloWorld",
+    headers: {"Connection": "close"},
+    https: false
+};
 
-/*var connection = thrift.createConnection("localhost", 6060, {
-    transport : transport,
-    protocol : protocol
-});*/
-var connection = thrift.createConnection("localhost", 6060);
+var connection = thrift.createHttpConnection("localhost", 6060, options);
+
+//var connection = thrift.createConnection("localhost", 6060);
 
 connection.on('error', function(err) {
+    if (err)  console.log(err);
     /*assert(false, err);*/
 });
 
-//var client = thrift.createClient(hello, connection);
-exports.client = thrift.createClient(hello, connection);
-    /*exports.hello = client.sayHello('roderick', function(err, response) {
+var client = thrift.createClient(hello, connection);
+//exports.client = thrift.createClient(hello, connection);
+client.sayHello('roderick', function(err, response) {
     console.log('sayHello()');
-    connection.end();});*/
+    connection.end();
+});
 
